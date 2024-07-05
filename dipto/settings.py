3cee3ceccc4c322e2cc54704249f310292b38176
oveshak <oveshaktoton9999@gmail.com>
2024-07-06 01:54:35 +0600

    first commit

--- dipto/settings.py
+++ dipto/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'dipto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'BzAqAYJMLHCylNRNmsMVsrtwqbUUDlRB',
-        'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '25527',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
